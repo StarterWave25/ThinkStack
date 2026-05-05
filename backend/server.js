3697c67691eff65ff7cf80bfb9111d8dd170832f
purushothamalli <purushotham.alli26@gmail.com>
2026-05-05 16:21:06 +0530

    Added httponly cookies with token in it and cookie-parser added

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-const connectDB = require('./src/lib/db');
-const authRouter = require('./src/routes/auth.router');
+const connectDB = require("./src/lib/db");
+const authRouter = require("./src/routes/auth.router");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Thinkify API!');
+app.get("/", (req, res) => {
+    res.send("Welcome to Thinkify API!");
 });
-app.use('/api/auth', authRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(process.env.PORT, () => {
-  console.log('\n\n--------------------');
-  console.log('⚡⚡ Server Running on PORT: ', process.env.PORT);
-  console.log('--------------------');
+    console.log("\n\n--------------------");
+    console.log("⚡⚡ Server Running on PORT: ", process.env.PORT);
+    console.log("--------------------");
 
-  connectDB();
+    connectDB();
 });
