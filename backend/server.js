ac122da184894dbdbfd03ac22598809c5366dd5f
prudvi <patnamprudvinath2006@gmail.com>
2026-05-08 20:01:41 +0530

    Now Bakckend also deployement ready.

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,12 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://thinkstack-tw.netlify.app/"
+    ], credentials: true
+}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
